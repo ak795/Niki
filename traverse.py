63a1e8b3f21189c8ec57516713bef162b5c9f3d3
ak795 <anishk795@gmail.com>
2017-01-04 19:37:59 +0530

    Final Script

--- traverse.py
+++ traverse.py
@@ -6,15 +6,23 @@ from glob import glob
 result = [y for x in os.walk(".") for y in glob(os.path.join(x[0], '*.txt'))]
 
 phone_numbers = []
-regex_str_phone = "[0-9]{1,3}\-\([0-9]{3}\)[0-9]{3}\-[0-9]{4}$"
-regex_str_US = "\+[0-9]{1,3}\-\([0-9]{3}\)\-[0-9]{3}\-[0-9]{4}$"
-regex_str_IND = "\+[0-9]{2}\-[0-9]{4}\-[0-9]{6}$"
-regex_str_IND_local = "[0-9]{10}$"
+regex_str_phone = "[0-9]{1,3}(\-|\ )\([0-9]{3}\)[0-9]{3}(\-|\ )[0-9]{4}$"         # xx-(xxx)xxx-xxxx
+regex_str_US = "\+[0-9]{1,3}(\-|\ )\([0-9]{3}\)(\-|\ )[0-9]{3}(\-|\ )[0-9]{4}$"   # +xx-(xxx)-xxx-xxxx
+regex_str_IND_1 = "\+[0-9]{2}(\-|\ )[0-9]{4}(\-|\ )[0-9]{6}$"                     # +xx-xxxx-xxxxxx
+regex_str_IND_2 = "\+[0-9]{2}(\-|\ )[0-9]{3}(\-|\ )[0-9]{7}$"                     # +xx-xxx-xxxxxxx
+regex_str_IND_3 = "\+[0-9]{2}(\-|\ )[0-9]{2}(\-|\ )[0-9]{8}$"                     # +xx-xx-xxxxxxxx
+regex_str_IND_mobile = "[0-9]{10}$"                                               # xxxxxxxxxx
+regex_str_IND_landline = "[0-9]{3}(\-|\ )[0-9]{8}"                                # xxx-xxxxxxxx
+regex_str_others = "^\+[1-9]{1}[0-9]{3,14}$"
 
 pattern_1 = re.compile(regex_str_phone)
 pattern_2 = re.compile(regex_str_US)
-pattern_3 = re.compile(regex_str_IND)
-pattern_4 = re.compile(regex_str_IND_local)
+pattern_3 = re.compile(regex_str_IND_1)
+pattern_4 = re.compile(regex_str_IND_mobile)
+pattern_5 = re.compile(regex_str_others)
+pattern_6 = re.compile(regex_str_IND_landline)
+pattern_7 = re.compile(regex_str_IND_2)
+pattern_8 = re.compile(regex_str_IND_3)
 
 # Case1: when data is structured and separated by comma's
 # Case2: when data is unstructured
@@ -23,13 +31,11 @@ pattern_4 = re.compile(regex_str_IND_local)
 
 for files in result:
     file = open(files , 'r')
-
     for lines in file:
         curr_line = lines.split(',')
 
         for p in curr_line:
-            print (p)
-            if pattern_1.match(p) or pattern_2.match(p) or pattern_3.match(p) or pattern_4.match(p):
+            if pattern_1.match(p) or pattern_2.match(p) or pattern_3.match(p) or pattern_4.match(p) or pattern_5.match(p) or pattern_6.match(p) or pattern_7.match(p) or pattern_8.match(p):
                 phone_numbers.append(p)
 
 
